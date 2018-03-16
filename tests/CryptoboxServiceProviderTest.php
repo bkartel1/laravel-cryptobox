695563ba7d639a4ae6bd47aeb4b12ecf27379642
Tuyen Pham <tuyenlaptrinh@gmail.com>
2018-03-17 02:06:47 +0700

    Update and rename CryptoboxServiceProviderTest.php.php to CryptoboxServiceProviderTest.php

---
+++ tests/CryptoboxServiceProviderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tuyenlaptrinh\Tests\Cryptobox;
+
+use Mockery;
+
+class CryptoboxServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRegister()
+    {
+        $this->assertTrue(true);
+    }
+}
