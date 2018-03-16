ab29026c565c4bdedff99bf237e2d24a522f565e
Tuyen Pham <tuyenlaptrinh@gmail.com>
2018-03-17 02:07:43 +0700

    Create CryptoboxTest.php

---
+++ tests/CryptoboxTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tuyenlaptrinh\Tests\Cryptobox;
+use Mockery;
+class CryptoboxTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $this->assertTrue(true);
+    }
+}
